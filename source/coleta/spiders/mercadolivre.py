a45605d687a7fa19671ae4a5ed60f9b63346af94
ClaudiorJr <claudiorsjunior@outlook.com>
2025-05-26 00:03:12 -0300

    Third  commit

--- source/coleta/spiders/mercadolivre.py
+++ source/coleta/spiders/mercadolivre.py
@@ -5,9 +5,13 @@ class MercadolivreSpider(scrapy.Spider):
     name = "mercadolivre"
     allowed_domains = ["lista.mercadolivre.com.br"]
     start_urls = ["https://lista.mercadolivre.com.br/tenis-corrida-masculino"]
+    max_pages = 10
 
     def parse(self, response):
+        
         produtos = response.css('div.poly-card__content')
+
+        current_page = response.meta.get('page', 1)
 
         for produto in produtos:
             
@@ -30,9 +34,24 @@ class MercadolivreSpider(scrapy.Spider):
                 'marca': produto.css('span.poly-component__brand::text').get(),
                 'nome': produto.css('a.poly-component__title::text').get(),
                 'preco_antigo_reais': precos[0] if len(precos) > 0 else None,
-                'preco_antigo_centavos': centavos[0] if len(precos) > 0 else None,
+                'preco_antigo_centavos': centavos[0] if len(centavos) > 0 else None,
                 'preco_novo_reais': precos[1] if len(precos) > 1 else None,
-                'preco_novo_centavos': centavos[1] if len(centavos) > 1 else None
+                'preco_novo_centavos': centavos[1] if len(centavos) > 1 else None,
+                'reviews_nota': produto.css('span.poly-reviews__rating::text').get(),
+                'quantidade_reviews': produto.css('span.poly-reviews__total::text').get()
             }
-            
-        
+
+        '''
+        Para seguir para as próximas páginas e continuar o scrapping, utilizamos o elemento do botão de próxima
+        página e ao final utilizamos "a::attr(href)" quedireciona para o link da próxima página
+        ''' 
+         # Paginação
+        if current_page < self.max_pages:
+            next_page = response.css('li.andes-pagination__button--next a::attr(href)').get()
+            if next_page:
+                yield scrapy.Request(
+                    url=next_page,
+                    callback=self.parse,
+                    meta={'page': current_page + 1},
+                    headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'}
+                )
